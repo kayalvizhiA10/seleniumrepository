f190757d4e0b55f2c96b4d0155febc1e3498f6d3
kayalvizhiA10 <73631699+kayalvizhiA10@users.noreply.github.com>
2023-02-08 11:47:58 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,17 @@
+package week1day1assign1;
+
+public class Bike {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Car bike =new Car();
+		bike.applybreak();
+		bike.soundhorn();
+		
+	
+		
+
+	}
+
+}
